6ba4f6dc9ff0f5088353a123c2106d9eb1c7c9f8
lvlieger <lvlieger@asu.edu>
2018-02-07 11:55:40 -0700

    Update index.ts to export user setting components

--- client/app/settings/index.ts
+++ client/app/settings/index.ts
@@ -1,1 +1,4 @@
 export * from './settings.component';
+export * from './change-username/change-username.component';
+export * from './change-name/change-name.component';
+export * from './change-password/change-password.component';
