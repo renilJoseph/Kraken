78279174ce4c8ac4bbbf09d9de10ffd1ecc99df5
lvlieger <lvlieger@asu.edu>
2018-02-07 11:56:07 -0700

    Update

--- client/app/settings/index.ts
+++ client/app/settings/index.ts
@@ -1,4 +1,2 @@
 export * from './settings.component';
-export * from './change-username/change-username.component';
-export * from './change-name/change-name.component';
-export * from './change-password/change-password.component';
+
