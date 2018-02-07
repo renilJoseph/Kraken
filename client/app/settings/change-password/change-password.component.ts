2f1146106eb0094a7329f5a011b9e98e875ba7dd
lvlieger <lvlieger@asu.edu>
2018-02-06 22:31:41 -0700

    (Update) User model -> email field (feat) Update User model field methods implemented

--- client/app/settings/change-password/change-password.component.ts
+++ client/app/settings/change-password/change-password.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService, UserService } from '../../_services/index';
+import { User } from '../../_models/index';
 
 @Component({
   moduleId: module.id,
   templateUrl: 'change-password.component.html',
 })
 
-export class ChangePasswordComponent implements OnInit{
-  constructor(){
+export class ChangePasswordComponent {
+  model: User;
+  currentUser: User;
+  loading = false;
+  
 
+  constructor(private userService: UserService, private alertService: AlertService){
+	this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+	this.model = this.currentUser;
   }
-  ngOnInit(){
 
+ 
+/*  get current user object and place into currentModel, copy it into model, then overwrite the object field from HTML5 form input (similar to register.component.html). 
+  changePassword() {
+      this.loading = true;
+      this.userService.update(this.model)
+          .subscribe(
+              data => {
+                  this.alertService.success('Password change successful.', true);
+              },
+              error => {
+                  this.alertService.error(error);
+                  this.loading = false;
+              });
   }
+*/
 }
